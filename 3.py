b9dc2117295ba2012f7e3bfd02943adc663c9c1e
sadxussein <kvdworking@gmail.com>
2023-06-06 23:41:56 +0300

    removed some garbage tests

--- 3.py
+++
@@ -1,4 +0,0 @@
-a = int(input())
-if a < -5: print("Low")
-elif 5 >= a >= -5: print("Mid")
-else: print("High")
--- 4.py
+++
@@ -1,4 +0,0 @@
-for i in "hello world":
-    if i == 'a':
-        break
-else: print('буквы а в строке нет')
