a691b579aa43c70f64792cd3342a428637de0b53
sadxussein <kvdworking@gmail.com>
2023-06-06 19:38:30 +0300

    finished with farm oop

--- farm_oop.py
+++ farm_oop.py
@@ -4,47 +4,55 @@ def spot(anim):
 class animal:   
     def __init__(self, name, weight):
         self.name = name
-        self.weight = weight        
-    
+        self.weight = weight
+        
     isFed = False
     
     def feed(self):
-        isFed = True    
+        isFed = True
         
 class bird(animal):
+    animal_class = "bird"        
     hasEggs = True
     def collect_eggs(self):
         hasEggs = False
         
 class mammal(animal):
+    animal_class = "mammal"
     isMilked = False
     def milk(self):
         isMilked = True
         
 class sheep(mammal):
-    isSreared = False
+    animal_type = "sheep"
+    isSheared = False
     def shear(self):
-        isSreared = True
+        isSheared = True
     def make_sound(self):
         print("baaaa")
         
 class cow(mammal):
+    animal_type = "cow"
     def make_sound(self):
         print("moooo")
         
 class goat(mammal):
+    animal_type = "goat"
     def make_sound(self):
         print("beeee")
 
 class chick(bird):
+    animal_type = "chick"
     def make_sound(self):
         print("cluck")
 
 class goose(bird):
+    animal_type = "goose"
     def make_sound(self):
         print("honk")
 
 class duck(bird):
+    animal_type = "duck"
     def make_sound(self):
         print("quack")
 
@@ -60,26 +68,15 @@ animals.append(goat("Копыта", 60))
 animals.append(sheep("Барашек", 65))
 animals.append(sheep("Кудрявый", 55))
   
-# for a in animals:
-    # a.feed()
-    # print(a.name, "is fed. Its weight is", a.weight)    
-    # if bird in a.__class__.__bases__ and a.hasEggs:
-        # a.collect_eggs()
-        # print("Collected eggs")
-    # if mammal in a.__class__.__bases__ and not a.isMilked:
-        # a.milk()
-        # print("Milked")
-    # if sheep in a.__class__ and not a.isSheared:
-        # a.shear()
-        # print("Sheared")
-    
-for base in animals[3].__class__.__bases__:
-    print(base.__name__)
-    
-print(bird)
-print(animals[3].__class__.__bases__)
-
-if bird == animals[3].__class__.__bases__:
-    print("pingus")
-else:
-    print("not pingus")
+for a in animals:
+    a.feed()
+    print(a.name, "is fed. Its weight is", a.weight)    
+    if a.animal_class == "bird" and a.hasEggs:
+        a.collect_eggs()
+        print("Collected eggs")
+    if a.animal_class == "mammal" and not a.isMilked:
+        a.milk()
+        print("Milked")
+    if a.animal_type == "sheep" and not a.isSheared:
+        a.shear()
+        print("Sheared")
