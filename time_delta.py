30f2d58372a45a66fab1f66110c36e3008a1ea76
sadxussein <kvdworking@gmail.com>
2023-06-22 23:42:49 +0300

    tests

--- time_delta.py
+++ time_delta.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import timedelta
 
 import math
 import os
@@ -6,22 +7,14 @@ import random
 import re
 import sys
 
-months = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
-
 def time_delta(t1, t2):
-    day = t1.split()
-    time = day[4].split(':')
-    print(day, time)
-    offset = datetime(0, 0, 0, int(day[5][1:3]), int(day[5][3:]))
-    print(offset)    
-    d1 = datetime(int(day[3]), int(months.get(day[2])), int(day[1], 10), int(time[0]), int(time[1]), int(time[2]))
-    d1 -= offset
-    print(d1)
-    day = t2.split()
-    print(day)
+    t1 = datetime.strptime(t1, "%a %d %b %Y %H:%M:%S %z")
+    t2 = datetime.strptime(t2, "%a %d %b %Y %H:%M:%S %z")
+    return str(int(abs(t2-t1).total_seconds()))
 
 n = int(input())
 for _ in range(n):
     t1 = input()
     t2 = input()
     delta = time_delta(t1, t2)
+    print(delta)
